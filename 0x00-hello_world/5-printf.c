d9457c13a4db956ad44c19e600b8530c34c69ff7
EssenceStar <enemali037@gmail.com>
2022-10-15 08:37:02 -0700

    program that prints exactly with proper grammar, but the outcome is a piece of art

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,11 +1,13 @@
 #include <stdio.h>
 
 /**
- * main - Entry point
- * Return: Always 0 (Successs)
+ * main - Prints "with proper grammer, but the outcome is a piece of art.
+ *
+ * Return: Always 0.
  */
-int mamin(void)
+int main(void)
 {
-	printf("with proper grammar, but the outcome is a piece of art,\n");
-	return(0);
+	printf("with proper grammer, but the outcome is a piece of art,/n");
+
+	return (0);
 }
